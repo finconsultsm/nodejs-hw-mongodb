913e1f4e51c5f9ccdbaf3e4f8b8601959f797187
Serhiy Malysh <166062683+finconsultsm@users.noreply.github.com>
2025-04-05 23:08:07 +0200

    1

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import authRouter from './routes/auth.js';
 import cookieParser from 'cookie-parser';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+app.use('/api-docs', swaggerDocs());
 app.use(authRouter);
 app.use(contactsRouter);
 
